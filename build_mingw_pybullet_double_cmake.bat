dd4ade06712b349f552d15399b4ef2266a8c4b1d
Hank Jordan <hank@henryjordan.com>
2018-04-27 12:11:15 -0400

    Tiny fix to make it work

--- build_mingw_pybullet_double_cmake.bat
+++ build_mingw_pybullet_double_cmake.bat
@@ -2,5 +2,5 @@
 echo If this file doesn't work for you, change the python version inside or install debug libraries for your python installation.
 mkdir build_cmake
 cd build_cmake
-cmake -DBUILD_PYBULLET=ON -DUSE_DOUBLE_PRECISION=ON -DCMAKE_BUILD_TYPE=Release -DPYTHON_INCLUDE_DIR=c:\python-3.5.3.amd64\include -DPYTHON_LIBRARY=c:\python-3.5.3.amd64\libs\python35.lib -DPYTHON_DEBUG_LIBRARY=c:\python-3.5.3.amd64\libs\python35_d.lib -G "MinGW Makefiles"
+cmake -DBUILD_PYBULLET=ON -DUSE_DOUBLE_PRECISION=ON -DCMAKE_BUILD_TYPE=Release -DPYTHON_INCLUDE_DIR=c:\python-3.5.3.amd64\include -DPYTHON_LIBRARY=c:\python-3.5.3.amd64\libs\python35.lib -DPYTHON_DEBUG_LIBRARY=c:\python-3.5.3.amd64\libs\python35_d.lib .. -G "MinGW Makefiles"
 mingw32-make
